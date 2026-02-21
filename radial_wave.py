9caa6bf5cea24921625af153b948e6402f8a3371
Toushif1611 <toushif1611@gmail.com>
2026-02-21 20:50:23 +0530

    Add Initial Files

--- radial_wave.py
+++ radial_wave.py
@@ -1,59 +1,58 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from mpl_toolkits.mplot3d import Axes3D
 
 # Range
-x = np.arange(-2, 2, 0.05)
-y = np.arange(-2, 2, 0.05)
-X, Y = np.meshgrid(x, y)
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
 
-# Constants
+# Constant
 A = 1.0
 K = 1.0
-W = 1.0
-T = 1.0
+w = 1.0
+t = 1.0
 
-# Initial wave
-Z = A * np.sin(K * np.sqrt(X**2 + Y**2) + W*T)
+X, Y = np.meshgrid(x, y)
+
+# Initail wave
+Z = A*np.sin(K*np.sqrt(X**2 + Y**2) + w*t)
 
 # Figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-plt.subplots_adjust(bottom=0.3)
 
-surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+ax.plot_surface(X, Y, Z, cmap='viridis')
 
-ax.set_title("Radial Wave")
+ax.view_init(elev=90, azim=90)
+
+# Slider
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
 
-# Sliders
 axA = plt.axes([0.2, 0.20, 0.6, 0.03])
 axK = plt.axes([0.2, 0.15, 0.6, 0.03])
 axW = plt.axes([0.2, 0.10, 0.6, 0.03])
 axT = plt.axes([0.2, 0.05, 0.6, 0.03])
 
-sliderA = Slider(axA, 'A', 0.1, 5.0, valinit=A)
-sliderK = Slider(axK, 'K', 0.1, 5.0, valinit=K)
-sliderW = Slider(axW, 'W', 0.1, 5.0, valinit=W)
-sliderT = Slider(axT, 'T', 0.1, 5.0, valinit=T)
+sliderA = Slider(axA, 'Amplitude', -5.0, 5.0, valinit=A)
+sliderK = Slider(axK, 'Wavelength', -5.0, 5.0, valinit=K)
+sliderW = Slider(axW, 'Frequency', -5.0, 5.0, valinit=w)
+sliderT = Slider(axT, 'Time', -5.0, 5.0, valinit=t)
 
-# Update function
+# Update Function
 def update(val):
     A = sliderA.val
     K = sliderK.val
-    W = sliderW.val
-    T = sliderT.val
+    w = sliderW.val
+    t = sliderT.val
+    
+    Z = A*np.sin(K*np.sqrt(X**2 + Y**2) + w*t)
     
     ax.clear()
-    Z = A * np.sin(K * np.sqrt(X**2 + Y**2) + W*T)
     ax.plot_surface(X, Y, Z, cmap='viridis')
-    ax.set_title("Radial Wave")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    fig.canvas.draw_idle()
 
 sliderA.on_changed(update)
 sliderK.on_changed(update)
